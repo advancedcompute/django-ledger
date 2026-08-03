6ebfdfd4fe52baf538e147e47454d10277b75f2f
advancedcompute <administrator@advancedcomputation.org.uk>
2026-08-03 01:44:51 +0100

    Bug fix attempt

--- django_ledger/migrations/0026_stagedtransactionmodel_customer_model_and_more.py
+++ django_ledger/migrations/0026_stagedtransactionmodel_customer_model_and_more.py
@@ -38,7 +38,7 @@ class Migration(migrations.Migration):
                 ('markdown_notes', models.TextField(blank=True, null=True, verbose_name='Markdown Notes')),
                 ('uuid', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('receipt_number', models.CharField(max_length=255, verbose_name='Receipt Number')),
-                ('receipt_date', models.DateField(verbose_name='Receipt Date'), default=get_localdate, null=True),
+                ('receipt_date', models.DateField(verbose_name='Receipt Date', default=get_localdate, null=True)),
                 ('receipt_type', models.CharField(max_length=255, choices=[('sales', 'Sales Receipt'), ('customer_refund', 'Sales Refund'), ('expense', 'Expense Receipt'), ('expense_refund', 'Expense Refund'), ('transfer', 'Transfer Receipt')], verbose_name='Receipt Type')),
                 ('amount', models.DecimalField(decimal_places=2, help_text='Amount of the receipt.', max_digits=20, validators=[django.core.validators.MinValueValidator(0)], verbose_name='Receipt Amount')),
                 ('charge_account', models.ForeignKey(help_text='The financial account (cash or credit) where this transaction was made.', on_delete=django.db.models.deletion.PROTECT, related_name='charge_receiptmodel_set', to='django_ledger.accountmodel', verbose_name='Charge Account')),
